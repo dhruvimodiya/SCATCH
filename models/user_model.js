8ef9024018e82e352daa3e8d266d787e95a3d9ce
dhruvimodiya-algo <dhruvimodiya.algobrainai@gmail.com>
2024-12-16 23:26:14 +0530

    16/12

---
+++ models/user_model.js
@@ -0,0 +1,27 @@
+// model basiclly set as variable and there datatype -like table 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/scatch');
+
+const userSchema = mongoose.Schema({
+    fullname : {
+        type : String,
+        minLength :3,
+        trim : true,
+    },
+    email : String,
+    password : String,
+    cart : {
+        type : Array,
+        default : [],
+    },
+    isadmin : Boolean,
+    orders : {
+        type : Array,
+        default : []
+    },
+    contact : Number,
+    picture : String,
+});
+
+module.exports = mongoose.module("user",userSchema);
